16b87811fa403f04c6befe7e57cd4489753d2824
kajolsinghme <kajolsingh985@gmail.com>
2026-05-25 00:22:48 +0530

    Implement store_chunks_in_chroma function for embedding storage in ChromaDB

--- app/services/vector_store_service.py
+++ app/services/vector_store_service.py
@@ -1,2 +1,20 @@
+from app.db.chroma_client import chroma_client
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def store_chunks_in_chroma(chunks):
-    
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-002",
+        google_api_key= os.getenv("GEMINI_API_KEY")
+    )
+
+    vector_store = Chroma(
+        collection_name="pdf_documents",
+        embedding_function=embeddings,
+        persist_directory="chroma_db"
+    )
