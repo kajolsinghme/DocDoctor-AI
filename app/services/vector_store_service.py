77023013693d1d30f49818678ee01b4f4bc1bc8d
kajolsinghme <kajolsingh985@gmail.com>
2026-05-26 22:07:32 +0530

    Implement store_chunks_in_chroma and create_documents functions for embedding storage and document creation

---
+++ app/services/vector_store_service.py
@@ -0,0 +1,35 @@
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def store_chunks_in_chroma(chunks, filename):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-002",
+        google_api_key=os.getenv("GEMINI_API_KEY")
+    )
+
+    vector_store = Chroma(
+        collection_name="pdf_documents",
+        embedding_function=embeddings,
+        persist_directory="chroma_db"
+    )
+    documents = create_documents(chunks, filename)
+
+    vector_store.add_documents(documents)
+
+def create_documents(chunks, filename):
+    documents = []
+    for chunk in chunks:
+        document = Document(
+            page_content=chunk["chunk"],
+            metadata={
+                "page": chunk["page"],
+                "source": filename
+            }
+        )
+        documents.append(document)
+    return documents
